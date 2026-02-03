58d1a7f9b7e2aff5b619d3c523c1d70674931b0f
Oleg <olegoandreev@yandex.ru>
2026-02-04 01:58:42 +0300

    Added debounce and withButtonDisabled utils

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,44 @@ export function getById<T extends HTMLElement>(elementId: string): T {
     }
     return element as T;
 }
+
+
+// Converts a function into a function which is run after a fixed timeout.
+//
+// Source code taken from https://tech.reverse.hr/articles/debounce-function-in-typescript
+export function debounce<T extends unknown[]>(
+    callback: (...args: T) => void,
+    delay: number,
+) {
+    let timeoutTimer: ReturnType<typeof setTimeout>;
+
+    return (...args: T) => {
+        clearTimeout(timeoutTimer);
+
+        timeoutTimer = setTimeout(() => {
+            callback(...args);
+        }, delay);
+    };
+}
+
+// Wraps an async event handler to disable a button while the handler is executing.
+export function withButtonDisabled<T extends Event>(
+    handler: (event: T) => Promise<void> | void,
+    button?: HTMLButtonElement,
+): (event: T) => Promise<void> {
+    return async (event: T) => {
+        if (!button) {
+            button = (event.target as HTMLButtonElement);
+        }
+        const wasDisabled = button.disabled;
+        button.disabled = true;
+        try {
+            await handler(event);
+        } finally {
+            // Only re-enable if it wasn't already disabled before
+            if (!wasDisabled) {
+                button.disabled = false;
+            }
+        }
+    };
+}
