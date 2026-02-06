2400f060cab3fe2a3b0aa96d9d54e2d8ec0fa094
Oleg <olegoandreev@yandex.ru>
2026-02-06 03:20:40 +0300

    Properly copy coi-serviceworker in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,29 @@
 import type { UserConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default {
   // For github pages
   base: '/pitch-changer/',
+
+  worker: {
+    format: 'es',
+  },
+
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/coi-serviceworker/coi-serviceworker.min.js",
+          dest: ".",
+        },
+      ],
+    })
+  ],
+
+  server: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin'
+    }
+  },
 } satisfies UserConfig
