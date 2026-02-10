81aa0b28e95e5154440d3bc0b1edd5cec8018bcd
Oleg <olegoandreev@yandex.ru>
2026-02-10 17:10:58 +0300

    Using fixed norm in spectrogram normalization

--- src/spectrogram.ts
+++ src/spectrogram.ts
@@ -1,5 +1,6 @@
 import initWasmModule, { Float32Vec, SpectralHistogram } from "../wasm/build/wasm_main_module";
 
+// Size of the FFT window for spectrogram computation
 export const SPECTROGRAM_SIZE = 2048;
 
 await initWasmModule();
@@ -8,6 +9,7 @@ const spectralHistogram = new SpectralHistogram(SPECTROGRAM_SIZE);
 const histogramInputVec = new Float32Vec(0);
 const histogramOutputVec = new Float32Vec(0);
 
+// Draw a spectrogram visualization of audio data onto a canvas
 export function drawSpectrogram(canvas: HTMLCanvasElement, audioData: Float32Array, numChannels: number) {
     histogramInputVec.set(audioData);
     spectralHistogram.compute(histogramInputVec, numChannels, histogramOutputVec);
@@ -16,8 +18,9 @@ export function drawSpectrogram(canvas: HTMLCanvasElement, audioData: Float32Arr
     const { width, height } = canvas.getBoundingClientRect();
     const canvasCtx = canvas.getContext('2d')!;
 
-    const maxValue = spectrogram.reduce((v1, v2) => Math.max(v1, v2), 0.0);
-    const norm = Math.min(maxValue, 100.0);
+    // We use fixed norm instead of computing the max value and coloring the whole spectrogram depending on it.
+    const NORM = 100.0;
+    // const maxValue = spectrogram.reduce((v1, v2) => Math.max(v1, v2), 0.0);
     // console.debug(`Got ${maxValue}`);
     canvasCtx.clearRect(0, 0, width, height);
 
@@ -26,13 +29,14 @@ export function drawSpectrogram(canvas: HTMLCanvasElement, audioData: Float32Arr
     const barWidth = width / spectrogramRange;
     canvasCtx.fillStyle = '#444444';
     for (let i = 0; i < spectrogramRange; i++) {
-        const barHeight = spectrogram[i] * height / norm;
+        const barHeight = spectrogram[i] * height / NORM;
 
         const x = i * barWidth;
         canvasCtx.fillRect(x, height - barHeight, barWidth, barHeight);
     }
 }
 
+// Clear the spectrogram canvas
 export function clearSpectrogram(canvas: HTMLCanvasElement) {
     const { width, height } = canvas.getBoundingClientRect();
     const canvasCtx = canvas.getContext('2d')!;
