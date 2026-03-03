fdb435a3c82279a35fbf6f4587de0b0d7144a4ea
Oleg <olegoandreev@yandex.ru>
2026-03-04 01:44:31 +0300

    Forgot add file to commit

--- wasm/src/phase_gradient_time_stretch.rs
+++ wasm/src/phase_gradient_time_stretch.rs
@@ -1,20 +1,19 @@
-use std::collections::BinaryHeap;
 use std::f32::consts::PI;
 use std::mem;
 
 use realfft::num_complex::Complex;
 
-use crate::util::{approx_atan2, approx_sincos};
+use crate::util::{approx_atan2, approx_sincos, normalize_phase};
 
-// PhaseGradientBin stored bin magnitude + bin index compacted into once i64 value. This allows much faster sorting of bins by
-// magnitude.
+// BinWithMagnitude stores bin magnitude + bin index compacted into once i64 value. This allows much faster sorting of
+// bins by magnitude.
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
-struct PhaseGradientBin {
+struct BinWithMagnitude {
     repr: i64,
 }
 
-impl PhaseGradientBin {
-    fn new(sqr_magnitude: f32, index: usize) -> Self {
+impl BinWithMagnitude {
+    fn new(index: usize, sqr_magnitude: f32) -> Self {
         let repr = (sqr_magnitude.to_bits() as i64) << 32 | index as i64;
         Self { repr }
     }
@@ -23,6 +22,7 @@ impl PhaseGradientBin {
         (self.repr & 0xFFFFFFFF) as usize
     }
 
+    #[allow(unused)]
     fn sqr_magnitude(&self) -> f32 {
         f32::from_bits((self.repr >> 32) as u32)
     }
@@ -44,12 +44,15 @@ pub struct PhaseGradientTimeStretch {
     sqr_magnitudes: Vec<f32>,
     ana_phases: Vec<f32>,
     syn_phases: Vec<f32>,
-    // Unlike the original paper, we split the max-heap into two heaps, where the first heap is simply a sorted vector
-    // of previous magnitudes. Heap operations take ~50% of all time in PitchShifter and we try to optimize them as much
-    // as we can. By segregating the HeapElem from prev stft frame and current stft frame, we reduce the BinaryHeap
-    // size and can use sort, which is faster than a bunch of push/pop operations.
-    prev_max_heap: Vec<PhaseGradientBin>,
-    max_heap: BinaryHeap<PhaseGradientBin>,
+    // Unlike the original paper, we do not use max-heap for all operations. Heap operations take more than ~50% of all
+    // time in PitchShifter and we try to optimize them as much as we can.
+    //
+    // Instead we slightly modify the original algorithm: we sort the bins from the previous frame, propagate the phase
+    // from the largest bin from the previous frame and then try to propagate the phase to the neighbors in the current
+    // frame.
+    //
+    // This results in almost the same phase assignment as the previous algorithm, but is considerably faster.
+    prev_sorted_bins: Vec<BinWithMagnitude>,
     // If true, syn_phases is assigned.
     phase_assigned: Vec<bool>,
 }
@@ -67,8 +70,7 @@ impl PhaseGradientTimeStretch {
         let sqr_magnitudes = vec![0.0; num_bins];
         let ana_phases = vec![0.0; num_bins];
         let syn_phases = vec![0.0; num_bins];
-        let prev_max_heap = Vec::with_capacity(num_bins);
-        let max_heap = BinaryHeap::with_capacity(num_bins);
+        let prev_sorted_bins = Vec::with_capacity(num_bins);
         let phase_assigned = vec![false; num_bins];
 
         Self {
@@ -80,8 +82,7 @@ impl PhaseGradientTimeStretch {
             sqr_magnitudes,
             ana_phases,
             syn_phases,
-            prev_max_heap,
-            max_heap,
+            prev_sorted_bins,
             phase_assigned,
         }
     }
@@ -94,8 +95,6 @@ impl PhaseGradientTimeStretch {
         syn_freq: &mut [Complex<f32>],
         syn_hop_size: usize,
     ) {
-        use crate::util::normalize_phase;
-
         // Ignore the frequencies with magnitude < (max magnitude * MIN_MAGNITUDE_TOLERANCE). Note that we deal with
         // squared magnitudes.
         const MIN_MAGNITUDE_TOLERANCE: f32 = 1e-6;
@@ -103,9 +102,11 @@ impl PhaseGradientTimeStretch {
         let num_bins = self.num_bins;
         assert_eq!(ana_freq.len(), num_bins);
         assert_eq!(syn_freq.len(), num_bins);
+        let ana_hop_size = ana_hop_size as f32;
+        let syn_hop_size = syn_hop_size as f32;
         // Original phase diff between frames for bin k = k * 2.0 * PI * ana_hop_size / fft_size.
-        let ana_phase_diff = 2.0 * PI * ana_hop_size as f32 / self.fft_size as f32;
-        let syn_phase_diff = 2.0 * PI * syn_hop_size as f32 / self.fft_size as f32;
+        let ana_phase_diff = 2.0 * PI * ana_hop_size / self.fft_size as f32;
+        let syn_phase_diff = 2.0 * PI * syn_hop_size / self.fft_size as f32;
 
         // Find maximum magnitude for thresholding
         let mut max_magn = 0.0f32;
@@ -117,22 +118,15 @@ impl PhaseGradientTimeStretch {
         }
         let min_magn = max_magn * MIN_MAGNITUDE_TOLERANCE;
 
-        //
+        self.prev_sorted_bins.clear();
 
-        self.prev_max_heap.clear();
-        self.max_heap.clear();
-        self.phase_assigned.fill(false);
-
-        // Number of false values in self.phase_assigned
-        let mut num_phase_unassigned = 0;
         for k in 0..num_bins {
             // Optimization: do not compute phase for frequencies below the min_magn threshold.
             if self.sqr_magnitudes[k] > min_magn {
                 // Use approximation instead of ana_freq[k].arg();
                 self.ana_phases[k] = approx_atan2(ana_freq[k].im, ana_freq[k].re);
                 self.phase_assigned[k] = false;
-                num_phase_unassigned += 1;
-                self.prev_max_heap.push(PhaseGradientBin::new(self.prev_sqr_magnitudes[k], k));
+                self.prev_sorted_bins.push(BinWithMagnitude::new(k, self.prev_sqr_magnitudes[k]));
             } else {
                 // The original paper assigns random values to frequencies below the min magnitude, but we simply zero
                 // them out.
@@ -141,66 +135,46 @@ impl PhaseGradientTimeStretch {
                 self.phase_assigned[k] = true;
             }
         }
-        self.prev_max_heap.sort_unstable();
+        self.prev_sorted_bins.sort_unstable();
 
-        // Phase assignment algorithm
-        while num_phase_unassigned > 0 {
-            let (k, from_prev) = self.find_next_heap_elem();
-            if from_prev {
-                // Element is from previous frame -- compute the phase based on previous frame only if the phase was not
-                // already propagated from neighbor (see next branch of this if).
-                if self.phase_assigned[k] {
-                    continue;
+        // Phase assignment algorithm: start with the bin with max magnitude.
+        for bin in (0..self.prev_sorted_bins.len()).rev() {
+            let k = self.prev_sorted_bins[bin].index();
+            if self.phase_assigned[k] {
+                continue;
+            }
+
+            // 1. Assign phase for bin k from previous frame.
+            self.assign_phase_from_prev(k, ana_hop_size, ana_phase_diff, syn_hop_size, syn_phase_diff);
+
+            // 2. Try propagating the phase to the smaller bins to the left.
+            let mut i = k;
+            while i > 0 {
+                if self.phase_assigned[i - 1] || self.sqr_magnitudes[i - 1] > self.sqr_magnitudes[i] {
+                    break;
                 }
-                self.phase_assigned[k] = true;
-                num_phase_unassigned -= 1;
-                self.max_heap.push(PhaseGradientBin::new(self.sqr_magnitudes[k], k));
-
-                // From the paper
-                //   φ_s(m_h, n) ← φ_s(m_h, n − 1) + a_s / 2 * ((∆_t φ_a) (m_h, n − 1) + (∆_t φ_a) (m_h, n))
-                //
-                // We simplify it by removing one of the derivatives to
-                //   φ_s(m_h, n) ← φ_s(m_h, n − 1) + a_s * (∆_t φ_a) (m_h, n),
-                // where the derivative is
-                //   (∆_t φ_a) (m_h, n) = 1 / a_a * [φ_a(m, n) − φ_a(m, n − 1) − 2πma_a / M]_2π + 2πm/M
-                //
-                // The idea is pretty simple: remove the "inherent" phase shift for the bin which is 2πk∆ / M where k is
-                // the bin size and ∆ is the difference in time (account for difference between analysis and synthesis
-                // time delta which is syn_hop_size - ana_hop_size). Then normalize phases to [-π, π) and compute
-                // the "residual" delta between two analysis phases (this happens e.g. when the true frequency differs
-                // from the bin frequency). After that interpolate (or extrapolate if syn_hop_size > ana_hop_size)
-                // the "residual" phase difference.
-                let ana_phase_derivative =
-                    normalize_phase(self.ana_phases[k] - self.prev_ana_phases[k] - ana_phase_diff * k as f32)
-                        / ana_hop_size as f32;
-                self.syn_phases[k] =
-                    self.prev_syn_phases[k] + ana_phase_derivative * syn_hop_size as f32 + syn_phase_diff * k as f32;
-            } else {
-                // Element is from current frame - propagate phase gradient to neighbors
-                if k > 0 && !self.phase_assigned[k - 1] {
-                    self.phase_assigned[k - 1] = true;
-                    num_phase_unassigned -= 1;
-                    self.max_heap.push(PhaseGradientBin::new(self.sqr_magnitudes[k - 1], k - 1));
-
-                    // From the paper
-                    //   φ_s(m_h + 1, n) ← φ_s(m_h, n) + b_s / 2 ((∆_f φ_a) (m_h, n) + (∆_f φ_a) (m_h + 1, n))
-                    //
-                    // We simplify it by removing one of the derivatives to
-                    //   φ_s(m_h + 1, n) ← φ_s(m_h, n) + b_s * (∆_f φ_a) (m_h, n),
-                    // where the derivative is
-                    //   (∆_f φ_a) (m_h, n) =  1 / b_a * [φ_a(m, n) − φ_a(m − 1, n)]_2π
-                    //
-                    // In simpler terms, we lock the phase in bin k-1 to phase in bin k.
-                    self.syn_phases[k - 1] = self.syn_phases[k] + self.ana_phases[k - 1] - self.ana_phases[k];
+                // Check if we should propagate phase from previous or current frame.
+                if self.sqr_magnitudes[i] >= self.prev_sqr_magnitudes[i - 1] {
+                    self.assign_phase_from_neighbor(i - 1, i);
+                } else {
+                    self.assign_phase_from_prev(i - 1, ana_hop_size, ana_phase_diff, syn_hop_size, syn_phase_diff);
                 }
+                i -= 1;
+            }
 
-                if k < self.num_bins - 1 && !self.phase_assigned[k + 1] {
-                    self.phase_assigned[k + 1] = true;
-                    num_phase_unassigned -= 1;
-                    self.max_heap.push(PhaseGradientBin::new(self.sqr_magnitudes[k + 1], k + 1));
-
-                    self.syn_phases[k + 1] = self.syn_phases[k] + self.ana_phases[k + 1] - self.ana_phases[k];
+            // 3. Try propagating the phase to the smaller bins to the right.
+            let mut i = k;
+            while i < num_bins - 1 {
+                if self.phase_assigned[i + 1] || self.sqr_magnitudes[i + 1] > self.sqr_magnitudes[i] {
+                    break;
                 }
+                // Check if we should propagate from previous or current frame.
+                if self.sqr_magnitudes[i] >= self.prev_sqr_magnitudes[i + 1] {
+                    self.assign_phase_from_neighbor(i + 1, i);
+                } else {
+                    self.assign_phase_from_prev(i + 1, ana_hop_size, ana_phase_diff, syn_hop_size, syn_phase_diff);
+                }
+                i += 1;
             }
         }
 
@@ -225,18 +199,54 @@ impl PhaseGradientTimeStretch {
         mem::swap(&mut self.prev_ana_phases, &mut self.ana_phases);
     }
 
-    // Find the next HeapElem with max magnited from prev_max_heap and max_heap, return true if it was from
-    // prev_max_heap and the bin index.
-    fn find_next_heap_elem(&mut self) -> (usize, bool) {
-        let prev_top_magnitude = self.prev_max_heap.last().map_or(-1.0, |e| e.sqr_magnitude());
-        let top_magnitude = self.max_heap.peek().map_or(-1.0, |e| e.sqr_magnitude());
-        if prev_top_magnitude > top_magnitude {
-            let top = self.prev_max_heap.pop().expect("INTERNAL ERROR: no more elements remaining in the heaps");
-            (top.index() as usize, true)
-        } else {
-            let top = self.max_heap.pop().expect("INTERNAL ERROR: no more elements remaining in the heaps");
-            (top.index() as usize, false)
-        }
+    // Helper that assigns the phase to bin k based on the phase of bin k in the previous frame.
+    //
+    // From the paper
+    //   φ_s(m_h, n) ← φ_s(m_h, n − 1) + a_s / 2 * ((∆_t φ_a) (m_h, n − 1) + (∆_t φ_a) (m_h, n))
+    //
+    // We simplify it by removing one of the derivatives to
+    //   φ_s(m_h, n) ← φ_s(m_h, n − 1) + a_s * (∆_t φ_a) (m_h, n),
+    // where the derivative is
+    //   (∆_t φ_a) (m_h, n) = 1 / a_a * [φ_a(m, n) − φ_a(m, n − 1) − 2πma_a / M]_2π + 2πm/M
+    //
+    // The idea is pretty simple: remove the "inherent" phase shift for the bin which is 2πk∆ / M where k is
+    // the bin size and ∆ is the difference in time (account for difference between analysis and synthesis
+    // time delta which is syn_hop_size - ana_hop_size). Then normalize phases to [-π, π) and compute
+    // the "residual" delta between two analysis phases (this happens e.g. when the true frequency differs
+    // from the bin frequency). After that interpolate (or extrapolate if syn_hop_size > ana_hop_size)
+    // the "residual" phase difference.
+    #[inline(always)]
+    fn assign_phase_from_prev(
+        &mut self,
+        k: usize,
+        ana_hop_size: f32,
+        ana_phase_diff: f32,
+        syn_hop_size: f32,
+        syn_phase_diff: f32,
+    ) {
+        let ana_phase_derivative =
+            normalize_phase(self.ana_phases[k] - self.prev_ana_phases[k] - ana_phase_diff * k as f32)
+                / ana_hop_size as f32;
+        self.syn_phases[k] =
+            self.prev_syn_phases[k] + ana_phase_derivative * syn_hop_size as f32 + syn_phase_diff * k as f32;
+        self.phase_assigned[k] = true;
+    }
+
+    // Helper that assigns the phase to bin k from another bin k_src in the current frame.
+    //
+    // From the paper
+    //   φ_s(m_h + 1, n) ← φ_s(m_h, n) + b_s / 2 ((∆_f φ_a) (m_h, n) + (∆_f φ_a) (m_h + 1, n))
+    //
+    // We simplify it by removing one of the derivatives to
+    //   φ_s(m_h + 1, n) ← φ_s(m_h, n) + b_s * (∆_f φ_a) (m_h, n),
+    // where the derivative is
+    //   (∆_f φ_a) (m_h, n) =  1 / b_a * [φ_a(m, n) − φ_a(m − 1, n)]_2π
+    //
+    // In simpler terms, we lock the phase in bin k_src to phase in bin k.
+    #[inline(always)]
+    fn assign_phase_from_neighbor(&mut self, k: usize, k_src: usize) {
+        self.syn_phases[k] = self.syn_phases[k_src] + (self.ana_phases[k] - self.ana_phases[k_src]);
+        self.phase_assigned[k] = true;
     }
 
     /// Reset internal state (clear previous frame data).
@@ -252,32 +262,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_phase_gradient_bin_binary_heap_ordering() {
-        // Test that BinaryHeap returns bins in descending magnitude order
-        let mut heap = BinaryHeap::new();
-
-        heap.push(PhaseGradientBin::new(1.5, 1));
-        heap.push(PhaseGradientBin::new(3.0, 2));
-        heap.push(PhaseGradientBin::new(0.5, 3));
-        heap.push(PhaseGradientBin::new(2.0, 4));
-
-        assert_eq!(heap.pop().unwrap().sqr_magnitude(), 3.0);
-        assert_eq!(heap.pop().unwrap().sqr_magnitude(), 2.0);
-        assert_eq!(heap.pop().unwrap().sqr_magnitude(), 1.5);
-        assert_eq!(heap.pop().unwrap().sqr_magnitude(), 0.5);
-        assert!(heap.is_empty());
-    }
-
-    #[test]
     fn test_phase_gradient_bin_sorting() {
-        // Test sorting of PhaseGradientBin vectors
+        // Test sorting of BinWithMagnitude vectors
         let mut bins = vec![
-            PhaseGradientBin::new(1.0, 0),
-            PhaseGradientBin::new(3.0, 1),
-            PhaseGradientBin::new(0.5, 2),
-            PhaseGradientBin::new(2.0, 3),
-            PhaseGradientBin::new(1.5, 4),
-            PhaseGradientBin::new(3.0, 5),
+            BinWithMagnitude::new(0, 1.0),
+            BinWithMagnitude::new(1, 3.0),
+            BinWithMagnitude::new(2, 0.5),
+            BinWithMagnitude::new(3, 2.0),
+            BinWithMagnitude::new(4, 1.5),
+            BinWithMagnitude::new(5, 3.0),
         ];
         bins.sort_unstable();
 
@@ -290,12 +283,12 @@ mod tests {
     #[test]
     fn test_phase_gradient_bin_edge_cases() {
         // Test with very large magnitude
-        let bin = PhaseGradientBin::new(f32::MAX, 10);
+        let bin = BinWithMagnitude::new(10, f32::MAX);
         assert_eq!(bin.sqr_magnitude(), f32::MAX);
         assert_eq!(bin.index(), 10);
 
         // Test with very small magnitude
-        let bin = PhaseGradientBin::new(f32::MIN_POSITIVE, 20);
+        let bin = BinWithMagnitude::new(20, f32::MIN_POSITIVE);
         assert_eq!(bin.sqr_magnitude(), f32::MIN_POSITIVE);
         assert_eq!(bin.index(), 20);
     }
