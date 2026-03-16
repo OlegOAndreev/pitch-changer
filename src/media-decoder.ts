1282a53e2da2b1f1ea06bdc8d5a880e07a7bfe33
Oleg <olegoandreev@yandex.ru>
2026-03-16 21:20:44 +0300

    Minor comment fix

--- src/media-decoder.ts
+++ src/media-decoder.ts
@@ -1,6 +1,5 @@
-// Decode an audio file from blob to interleaved PCM array with sample rate, the result is backed by SharedArrayBuffer. We must
-// use a real AudioContext, not OfflineAudioContext, because we want to get data automatically resampled into playable
-// sample rate.
+// Decode an audio file from blob to interleaved PCM array with sample rate. We must use a real AudioContext, not
+// OfflineAudioContext, because we want to get data automatically resampled into playable sample rate.
 import type { InterleavedAudio } from './types';
 
 // Decode audio blob to interleaved PCM using AudioContext
@@ -10,8 +9,7 @@ export async function decodeAudioFromBlob(blob: Blob, audioContext: AudioContext
 
     const numChannels = audioBuffer.numberOfChannels;
     const samplesPerChannel = audioBuffer.length;
-    const resultSab = new SharedArrayBuffer(numChannels * samplesPerChannel * 4);
-    const data = new Float32Array(resultSab);
+    const data = new Float32Array(numChannels * samplesPerChannel);
 
     for (let ch = 0; ch < numChannels; ch++) {
         const channelData = audioBuffer.getChannelData(ch);
