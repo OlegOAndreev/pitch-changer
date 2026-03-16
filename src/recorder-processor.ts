eee7ceaf8de61efd5777ee32a52206f12ef7c3d6
Oleg <olegoandreev@yandex.ru>
2026-03-16 21:33:24 +0300

    Cleaned up RecorderProcessor

--- src/recorder-processor.ts
+++ src/recorder-processor.ts
@@ -3,27 +3,21 @@
 import { recorderProcessorName, type RecordedSamplesMessage } from './recorder-types';
 
 class RecorderProcessor extends AudioWorkletProcessor {
-    private isRecording: boolean = true;
-
     constructor() {
         super();
     }
 
     process(inputs: Float32Array[][], _outputs: Float32Array[][], _parameters: Record<string, Float32Array>): boolean {
-        if (!this.isRecording) {
-            return false;
-        }
-
         const input = inputs[0];
         if (input && input.length > 0) {
             // Get the first channel (mono recording)
             const channelData = input[0];
-            const audioData = new Float32Array(channelData.length);
-            audioData.set(channelData);
+            const samples = new Float32Array(channelData.length);
+            samples.set(channelData);
             const message: RecordedSamplesMessage = {
-                samples: audioData,
+                samples: samples,
             };
-            this.port.postMessage(message, [audioData.buffer]);
+            this.port.postMessage(message, [samples.buffer]);
         }
 
         return true;
